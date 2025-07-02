794881805f634c26306df798d00712f265ba60d2
Liam Egan <liam@wethecollective.com>
2025-07-02 14:05:15 -0700

    Adding metadata function to article page

--- src/app/(site)/posts/[slug]/page.tsx
+++ src/app/(site)/posts/[slug]/page.tsx
@@ -1,5 +1,8 @@
+import type { Metadata } from "next";
+
 import { sanityFetch } from "@/sanity/lib/live";
 import { groq } from "next-sanity";
+import { buildSrc } from "@sanity-image/url-builder";
 import { PortableText, PortableTextBlock } from "next-sanity";
 
 import { image } from "@/components/image";
@@ -11,6 +14,11 @@ interface Post {
     title: string;
     publishedAt: string;
     body: PortableTextBlock[];
+    mainImage?: {
+      asset: {
+        _ref: string;
+      };
+    };
   };
 }
 
@@ -22,6 +30,37 @@ const components = {
   },
 };
 
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const query = groq`*[ _type == "post" && slug.current == $slug ][0] {
+    _id, title, publishedAt, mainImage
+  }`;
+
+  const post: Post = await sanityFetch({ query, params });
+
+  let src = null;
+  if (post?.data?.mainImage?.asset?._ref) {
+    ({ src } = buildSrc({
+      id: post.data.mainImage?.asset._ref ?? "",
+      width: 1200,
+      baseUrl: `https://cdn.sanity.io/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/${process.env.NEXT_PUBLIC_SANITY_DATASET}/`,
+    }));
+  }
+
+  console.log(src);
+
+  return {
+    title: post.data.title,
+    description: `Published on ${new Date(post.data.publishedAt).toDateString()}`,
+    openGraph: {
+      images: src ? [src] : [],
+    },
+  };
+}
+
 export default async function PostPage({
   params,
 }: {
@@ -30,12 +69,14 @@ export default async function PostPage({
   const { slug } = await params;
 
   const query = groq`*[ _type == "post" && slug.current == $slug ][0] {
-    _id, title, body, publishedAt
+    _id, title, body, publishedAt, mainImage
   }`;
 
   const post: Post = await sanityFetch({ query, params });
 
   if (!post?.data) return <div>No post found for {slug}</div>;
+
+  console.log("Post data:", post.data);
 
   return (
     <article>
